9e86b66d13efcdb1a77ab435b5c6756a0d3e56b2
SadeeMsr <srrohan333@gmail.com>
2023-12-17 16:53:13 +0600

    cross browser origin package added

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import routes from './Routes/index.js'
 const app = express();
 
 
 app.use(express.json())
 // app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin: 'http://127.0.0.1:5173',
+}));
+
+
 app.use(morgan('combined'))
 
 app.get("/", (req, res) => {
